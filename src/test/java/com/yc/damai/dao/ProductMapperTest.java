9090054e3a301e1b66b1ebe25c7c75c309f336e2
swcba <2463079095@qq.com>
2019-10-31 20:19:39 +0800

    log4j日志的输出

--- src/test/java/com/yc/damai/dao/ProductMapperTest.java
+++ src/test/java/com/yc/damai/dao/ProductMapperTest.java
@@ -8,14 +8,17 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.yc.damai.bean.Product;
 
 public class ProductMapperTest {
 	
-	@Test
-	public void testSelectAll() throws IOException{
+	private SqlSession  session;
+
+	@Before
+	public void before() throws IOException{
 		// 初始化 MyBatis 矿建
 		String resource = "mybatis.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -23,11 +26,22 @@ public class ProductMapperTest {
 		// SqlSessionFactory  会话工厂
 		// SqlSession   会话
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		SqlSession session = sqlSessionFactory.openSession();
+		session = sqlSessionFactory.openSession();
+	}
+	
+	@Test
+	public void testSelectAll() throws IOException{
 		List<Product> list = session.selectList("com.yc.damai.dao.ProductMapper.selectAll");
 		System.out.println(list.size());
-		
 		System.out.println(list.get(0));
 	}
+	
+	
+	@Test
+	public void testSelectByPid() throws IOException{
+		Product p = session.selectOne("com.yc.damai.dao.ProductMapper.selectByPid", 56);
+		System.out.println(p);
+	}
+
 
 }
